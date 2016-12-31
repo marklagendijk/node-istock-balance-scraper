6d7d58727a2da3880745a4b502f373445fe52cd0
Mark Lagendijk <mark@lagendijk.nl>
2016-12-31 01:31:47 +0100

    Get balance from financialTab, because it isn't on the bar anymore.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const Promise = require('bluebird');
 const pm2Starter = require('./lib/pm2-starter');
 const iStockApi = require('./lib/istock-api');
 const poller = require('./lib/poller');
-const config = require('./lib/config').init();
+const config = require('./lib/config');
 
-var argv = require('yargs')
+const argv = require('yargs')
     .usage('Usage: istock-balance-scraper <command>')
     .command('config', 'Initialize the config of the scraper.')
     .command('pm2', 'Start the scraper in PM2.')
@@ -16,16 +16,18 @@ var argv = require('yargs')
 
 switch (argv[0]) {
 case 'pm2':
+    config.init();
     pm2Starter.startAndSave();
     break;
 
 case 'direct':
+    config.init();
     Promise.resolve()
         .then(() => iStockApi.login(config.credentials))
         .then(() => poller.start());
     break;
 
 case 'config':
-    config.creatUserConfig();
+    config.createUserConfig();
     break;
 }
